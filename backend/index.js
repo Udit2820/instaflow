0782c4c6ea46a73cfbe71dba24bbb938d3f63aec
Udit Pratap Singh <udit2962@gmail.com>
2024-09-04 21:15:32 +0530

    first final

--- backend/index.js
+++ backend/index.js
@@ -9,9 +9,13 @@ import postRoute from "./routes/post.route.js";
 import messageRoute from "./routes/message.route.js";
 import { app, server,  } from "./socket/socket.js";
 
+import path from "path";
 dotenv.config({});
 
 const PORT = process.env.PORT || 3000;
+const __dirname = path.resolve();
+// console.log(__dirname);
+
 
 app.get("/",(_,res)=>{
     return res.status(200).json({
@@ -34,7 +38,14 @@ app.use(cors(corsOptions));
 app.use("/api/v2/user",userRoute);
 app.use("/api/v2/post",postRoute);
 app.use("/api/v2/message",messageRoute);
-// "http://localhost:800/api/v1/user/register"n
+// "http://localhost:800/api/v1/user/register"
+
+app.use(express.static(path.join(__dirname,"/frontend/dist")));
+app.get("*",(req,res)=>{
+    res.sendFile(path.resolve(__dirname,"frontend","dist","index.html"))
+})
+
+
 server.listen(PORT,()=>{
     connectDB();
     console.log(`Server is running on port ${PORT}`);
