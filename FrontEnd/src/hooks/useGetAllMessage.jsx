59f0b44889c664c5cfbfa8d9d1bb1f3163909a5b
Udit Pratap Singh <udit2962@gmail.com>
2024-09-05 00:39:10 +0530

    ready to deploy

--- FrontEnd/src/hooks/useGetAllMessage.jsx
+++ FrontEnd/src/hooks/useGetAllMessage.jsx
@@ -11,8 +11,7 @@ const useGetAllMessage = ()=>{
         const fetchAllMessage = async()=>{
             try{
                 const res= await axios.get(`http://localhost:8000/api/v2/message/all/${selectedUser?._id}`,{withCredentials:true});
-                if(res.data.success){
-                    debugger;
+                if(res.data.succes){
                     dispatch(setMessages(res.data.messages));
                     // console.log(res.data)
                 }
