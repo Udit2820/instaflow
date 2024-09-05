19384aa19f7226781616f4bd552d7f47f450297e
Udit Pratap Singh <udit2962@gmail.com>
2024-09-06 01:55:32 +0530

    Socket

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -56,7 +56,7 @@ function App() {
   const dispatch = useDispatch();
   useEffect(()=>{
     if(user){
-      const socketio = io('https://instaflow.onrender.com',{
+      const socketio = io('http://localhost:8000',{
         query:{
           userId:user?._id,
         },
